524aa58165f67b4c79557f09ce30fb610fab5862
PatoAh <98765285+vilmapato@users.noreply.github.com>
2025-12-17 05:18:06 +0100

    did string 3 exercise

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,17 +1,20 @@
+use std::fmt::Formatter;
+
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
-    while let i =  {
+    input.trim()
         
-    }
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    format!("{} world!", input)
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
-}
+    input.replace("cars", "balloons")
+}   
 
 fn main() {
     // You can optionally experiment here.
